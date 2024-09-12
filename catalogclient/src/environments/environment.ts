7986667e3da86cf6da1c78dd7e4c9f7a52297aad
akjal <jaleelak@gmail.com>
2024-09-12 17:40:19 +0530

    Update environment.ts

--- catalogclient/src/environments/environment.ts
+++ catalogclient/src/environments/environment.ts
@@ -1,14 +1,15 @@
 export const environment = {
-  production: false,
-  msalConfig: {
+  production: true,
+ msalConfig: {
     auth: {
-      clientId: 'ENTER_CLIENT_ID',
-      authority: 'ENTER_AUTHORITY',
+      clientId: '95fe6126-b3bc-4611-86a9-024db3a83db6',
+      authority:
+        'https://login.microsoftonline.com/db71a4d9-f264-488b-8ba3-b07c26caea6a',
     },
   },
   apiConfig: {
-    scopes: ['ENTER_SCOPE'],
-    uri: 'ENTER_URI',
-    catalogApiUri: 'https://localhost:5001',
+    scopes: ['user.read'],
+    uri: 'https://graph.microsoft.com/v1.0/me',
+    catalogApiUri: 'https://bigdreamsapps-new-ewanhuhyb4gsfddh.eastus-01.azurewebsites.net/api/',
   },
 };
